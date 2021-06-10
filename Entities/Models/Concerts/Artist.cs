aad4308304c3b8bf517c9a472c59fa70d7bbb6d1
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-06-10 18:32:27 +0300

    Create Artist model

---
+++ Entities/Models/Concerts/Artist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models.Concerts
+{
+    public class Artist
+    {
+        [Column("ArtistId")]
+        public Guid Id { get; set; }
+        
+        [Required(ErrorMessage = "Artist name is a required field.")]
+        [MaxLength(60, ErrorMessage = "Maximum length for the Artist Name is 60 characters.")]
+        public string GenreName { get; set; }
+    }
+}
