3137ed2105a08dc60de86a594270dca7534e7f80
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-06-10 18:32:36 +0300

    Create Genre model

---
+++ Entities/Models/Concerts/Genre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models.Concerts
+{
+    public class Genre
+    {
+        [Column("GenreId")]
+        public Guid Id { get; set; }
+        
+        [Required(ErrorMessage = "Genre name is a required field.")]
+        [MaxLength(30, ErrorMessage = "Maximum length for the Genre Name is 30 characters.")]
+        public string GenreName { get; set; }
+    }
+}
