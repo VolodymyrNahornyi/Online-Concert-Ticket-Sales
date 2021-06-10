ad6559372286fc86caeba0d51c0958fa5da831b6
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-06-10 18:58:08 +0300

    Create RepositoryContext class with Artists and Genres DbSets

---
+++ Entities/RepositoryContext.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Models.Concerts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entities
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options)
+        {
+        }
+        
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
