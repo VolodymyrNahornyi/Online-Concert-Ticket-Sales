cdd9367d12efb777fef9e0aa30f49a159e601ec6
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-06-11 18:11:32 +0300

    Add AddGenreEnumProperty migration

--- OnlineConcertTicketSales/Migrations/20210611150851_AddGenreEnumProperty.Designer.cs
+++ OnlineConcertTicketSales/Migrations/20210611150851_AddGenreEnumProperty.Designer.cs
@@ -4,14 +4,16 @@ using Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace OnlineConcertTicketSales.Migrations
 {
     [DbContext(typeof(RepositoryContext))]
-    partial class RepositoryContextModelSnapshot : ModelSnapshot
+    [Migration("20210611150851_AddGenreEnumProperty")]
+    partial class AddGenreEnumProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ OnlineConcertTicketSales/Migrations/20210611150851_AddGenreEnumProperty.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineConcertTicketSales.Migrations
+{
+    public partial class AddGenreEnumProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "GenreName",
+                table: "Genres",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "GenreName",
+                table: "Genres",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldMaxLength: 30);
+        }
+    }
+}
