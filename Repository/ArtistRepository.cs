2c748c358f0ef19c65e28ee0321ceda91c2c8a06
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-06-17 14:41:36 +0300

    Create concrete artist repository class

---
+++ Repository/ArtistRepository.cs
@@ -0,0 +1,13 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models.Concerts;
+
+namespace Repository
+{
+    public class ArtistRepository : RepositoryBase<Artist>, IArtistRepository
+    {
+        public ArtistRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+        {
+        }
+    }
+}
