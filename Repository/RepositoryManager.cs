61be8792f9616678c7f6f796740451a2b66d2eca
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-06-17 15:09:01 +0300

    Create RepositoryManager class implementing IRepositoryManager interface

---
+++ Repository/RepositoryManager.cs
@@ -0,0 +1,45 @@
+﻿using Contracts;
+using Entities;
+
+namespace Repository
+{
+    public class RepositoryManager : IRepositoryManager
+    {
+        private readonly RepositoryContext _repositoryContext;
+
+        private IGenreRepository _genreRepository;
+        private IArtistRepository _artistRepository;
+
+        public RepositoryManager(RepositoryContext repositoryContext)
+        {
+            _repositoryContext = repositoryContext;
+        }
+
+        public IGenreRepository Genre
+        {
+            get
+            {
+                if (_genreRepository == null)
+                    _genreRepository = new GenreRepository(_repositoryContext);
+
+                return _genreRepository;
+            }
+        }
+
+        public IArtistRepository Artist
+        {
+            get
+            {
+                if (_artistRepository == null)
+                    _artistRepository = new ArtistRepository(_repositoryContext);
+
+                return _artistRepository;
+            }
+        }
+
+        public void Save()
+        {
+            _repositoryContext.SaveChanges();
+        }
+    }
+}
