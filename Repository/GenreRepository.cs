5d710cb83c3383f2322adf2a71504f4df5fe1e07
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-06-17 14:39:30 +0300

    Create concrete genre repository class

---
+++ Repository/GenreRepository.cs
@@ -0,0 +1,13 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models.Concerts;
+
+namespace Repository
+{
+    public class GenreRepository : RepositoryBase<Genre>, IGenreRepository
+    {
+        public GenreRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+        {
+        }
+    }
+}
