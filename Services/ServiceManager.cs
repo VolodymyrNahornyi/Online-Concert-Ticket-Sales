f168bacd3b75e1bb6cd38b2304db62df0d8569dd
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-06-18 09:34:40 +0300

    Create IServiceManager class implementing IServiceManager interface

---
+++ Services/ServiceManager.cs
@@ -0,0 +1,41 @@
+﻿using Contracts;
+
+namespace Services
+{
+    public class ServiceManager : IServiceManager
+    {
+        private readonly IRepositoryManager _repositoryManager;
+
+
+        public ServiceManager(IRepositoryManager repositoryManager)
+        {
+            _repositoryManager = repositoryManager;
+        }
+
+        private IGenreService _genreService;
+        private IArtistService _artistService;
+
+        public IGenreService Genre
+        {
+            get
+            {
+                if (_genreService == null)
+                    _genreService = new GenreService(_repositoryManager);
+
+                return _genreService;
+            }
+        }
+        
+        public IArtistService Artist
+        {
+            get
+            {
+                if (_artistService == null)
+                    _artistService = new ArtistService(_repositoryManager);
+
+                return _artistService;
+            }
+        }
+
+    }
+}
