6ff0efcc224ba24f8e64b3d7319191b0583b3100
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-06-18 09:17:15 +0300

    Create GenreService and ArtistService classes

---
+++ Services/ArtistService.cs
@@ -0,0 +1,14 @@
+﻿using Contracts;
+
+namespace Services
+{
+    public class ArtistService : IArtistService
+    {
+        private readonly IRepositoryManager _repositoryManager;
+
+        public ArtistService(IRepositoryManager repositoryManager)
+        {
+            _repositoryManager = repositoryManager;
+        }
+    }
+}
---
+++ Services/GenreService.cs
@@ -0,0 +1,14 @@
+﻿using Contracts;
+
+namespace Services
+{
+    public class GenreService : IGenreService
+    {
+        private readonly IRepositoryManager _repositoryManager;
+
+        public GenreService(IRepositoryManager repositoryManager)
+        {
+            _repositoryManager = repositoryManager;
+        }
+    }
+}
