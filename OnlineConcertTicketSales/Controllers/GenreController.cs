219f91d16ec2a9c58039194c2416baf554bdb9d0
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-06-19 09:58:25 +0300

    Create [HttpGet] GetGenres method in the GenreController class

---
+++ OnlineConcertTicketSales/Controllers/GenreController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineConcertTicketSales.Controllers
+{
+    [Route("api/genres")]
+    [ApiController]
+    public class GenreController : ControllerBase
+    {
+        private readonly IServiceManager _serviceManager;
+        private readonly ILoggerManager _logger;
+
+        public GenreController(IServiceManager serviceManager, ILoggerManager loggerManager)
+        {
+            _serviceManager = serviceManager;
+            _logger = loggerManager;
+        }
+
+        [HttpGet]
+        public IActionResult GetGenres()
+        {
+            try
+            {
+                var genres = _serviceManager.Genre.GetAllGenres(false);
+                return Ok(genres);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong in the {nameof(GetGenres)} action {ex}");
+            return StatusCode(500, "Internal server error");
+            }
+        }
+        
+    }
+}
