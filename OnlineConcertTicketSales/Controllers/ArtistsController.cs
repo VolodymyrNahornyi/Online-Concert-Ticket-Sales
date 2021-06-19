82752fc55b67a2a499a0c2faba17f07227231df3
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-06-19 13:52:21 +0300

    Create ArtistsController

---
+++ OnlineConcertTicketSales/Controllers/ArtistsController.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineConcertTicketSales.Controllers
+{
+    [Route("api/genres/{genreId}/artists")]
+    [ApiController]
+    public class ArtistsController : ControllerBase
+    {
+        private readonly IServiceManager _serviceManager;
+        private readonly ILoggerManager _logger;
+        private readonly IMapper _mapper;
+
+        public ArtistsController(IServiceManager serviceManager, ILoggerManager loggerManager, IMapper mapper)
+        {
+            _serviceManager = serviceManager;
+            _logger = loggerManager;
+            _mapper = mapper;
+        }
+    }
+}
