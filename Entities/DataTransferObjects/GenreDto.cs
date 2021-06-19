194390c3d4d9fb1150da4ee38cbaa2dda7b9375c
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-06-19 10:59:55 +0300

    Create GenreDto class

---
+++ Entities/DataTransferObjects/GenreDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Entities.DataTransferObjects
+{
+    public class GenreDto
+    {
+        public Guid Id { get; set; }
+        public string GenreName { get; set; }
+    }
+}
