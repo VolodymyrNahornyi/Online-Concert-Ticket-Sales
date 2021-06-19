2a679a9d23795db747c22c9c91f5f7991c4b4331
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-06-19 12:20:20 +0300

    Create MappingProfile class and add the rule for mapping Genre and GenreDto objects

---
+++ OnlineConcertTicketSales/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Entities.DataTransferObjects;
+using Entities.Models.Concerts;
+
+namespace OnlineConcertTicketSales
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Genre, GenreDto>();
+        }
+    }
+}
