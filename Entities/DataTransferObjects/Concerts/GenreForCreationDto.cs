b747ee45abc1fa3e346f69f0cb0acbcddb471042
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-06-20 16:15:34 +0300

    Create GenreForCreationDto model class

---
+++ Entities/DataTransferObjects/Concerts/GenreForCreationDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Entities.Models.Concerts;
+
+namespace Entities.DataTransferObjects
+{
+    public class GenreForCreationDto
+    {
+        public GenreName GenreName { get; set; }
+    }
+}
