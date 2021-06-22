e6f6aaf5dcac1d61afcf2db9fb5a0d6c9f6b562e
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-06-22 15:57:14 +0300

    Create custom EnumFieldAttribute class

---
+++ Entities/ValidationAttributes/RequiredEnumFieldAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace Entities.ValidationAttributes
+{
+    public class RequiredEnumFieldAttribute : RequiredAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+
+            var type = value.GetType();
+            return type.IsEnum && Enum.IsDefined(type, value);
+        }
+    }
+}
