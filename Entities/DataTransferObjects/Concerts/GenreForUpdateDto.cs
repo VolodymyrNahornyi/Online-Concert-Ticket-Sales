836e2686cbea87b464628ca912268a111cfbd028
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-06-22 13:19:33 +0300

    Create GenreForUpdateDto model class

---
+++ Entities/DataTransferObjects/Concerts/GenreForUpdateDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Entities.Models.Concerts;
+
+namespace Entities.DataTransferObjects
+{
+    public class GenreForUpdateDto
+    {
+        public GenreName GenreName { get; set; }
+        
+        public IEnumerable<ArtistForCreationDto> Artists { get; set; }
+    }
+}
