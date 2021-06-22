c93125b7e50d5cece20b39e839a4e5892723efd3
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-06-22 15:12:33 +0300

    Add DataAnnotation attribute to the ArtistForCreationDto model

--- Entities/DataTransferObjects/Concerts/ArtistForCreationDto.cs
+++ Entities/DataTransferObjects/Concerts/ArtistForCreationDto.cs
@@ -1,7 +1,11 @@
-﻿namespace Entities.DataTransferObjects
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities.DataTransferObjects
 {
     public class ArtistForCreationDto
     {
+        [Required(ErrorMessage = "Artist name is a required field.")]
+        [MaxLength(30, ErrorMessage = "Maximum length for the Artist name is 30 characters.")]
         public string ArtistName { get; set; }
     }
 }
