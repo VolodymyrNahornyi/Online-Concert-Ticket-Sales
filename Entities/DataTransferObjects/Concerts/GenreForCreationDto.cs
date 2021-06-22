5210d2ec4406e4716298449834f109e710c38081
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-06-22 15:57:55 +0300

    Add RequiredEnumField attribute annotation to the GenreName field

--- Entities/DataTransferObjects/Concerts/GenreForCreationDto.cs
+++ Entities/DataTransferObjects/Concerts/GenreForCreationDto.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Entities.Models.Concerts;
+using Entities.ValidationAttributes;
 
 namespace Entities.DataTransferObjects
 {
     public class GenreForCreationDto
     {
+        [RequiredEnumField(ErrorMessage = "Genre name is a required field!")]
         public GenreName GenreName { get; set; }
+        
         
         public IEnumerable<ArtistForCreationDto> Artists { get; set; }
     }
