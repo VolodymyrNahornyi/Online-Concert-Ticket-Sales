a74f3a3fd1de79234de8083c2d5561ad9d316951
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-06-24 18:02:52 +0300

    Create IDataShaper<T> and implement it in DataShaper<T> class

---
+++ Contracts/IDataShaper.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+
+namespace Contracts
+{
+    public interface IDataShaper<T>
+    {
+        IEnumerable<ExpandoObject> ShapeData(IEnumerable<T> entities, string fieldsString);
+        ExpandoObject ShapeData(T entity, string fieldsString);
+    }
+}
