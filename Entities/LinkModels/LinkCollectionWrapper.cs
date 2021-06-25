3fb154605635034a5578517659abdddc56110cd6
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-06-25 10:21:32 +0300

    Create Link Models for implementing HATEOAS

---
+++ Entities/LinkModels/LinkCollectionWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Entities.LinkModels
+{
+    public class LinkCollectionWrapper<T> : LinkResourceBase
+    {
+        public List<T> Value { get; set; } = new List<T>();
+        public LinkCollectionWrapper()
+        {}
+        public LinkCollectionWrapper(List<T> value)
+        {
+            Value = value;
+        }
+    }
+}
