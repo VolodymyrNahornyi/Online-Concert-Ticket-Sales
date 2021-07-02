5bd5fc410b8a97d0cb6b23082a56169da29a0ec8
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-07-02 17:24:44 +0300

    Create GetConcertsAsync method in ConcertRepository class

---
+++ Repository/ConcertRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contracts;
+using Entities;
+using Entities.Models.Concerts;
+using Entities.RequestFeatures;
+using Microsoft.EntityFrameworkCore;
+using Repository.Extensions;
+
+namespace Repository
+{
+    public class ConcertRepository : RepositoryBase<Concert>, IConcertRepository
+    {
+        public ConcertRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+        {
+        }
+
+        public async Task<PagedList<Concert>> GetConcertsAsync(Guid genreId, Guid artistId, ConcertParameters concertParameters, bool trackChanges)
+        {
+            var concerts = await FindByCondition(c => c.Artist.GenreId.Equals(genreId) && c.ArtistId.Equals(artistId), trackChanges)
+                .ToListAsync();
+            
+            return PagedList<Concert>.ToPagedList(concerts, concertParameters.PageNumber, concertParameters.PageSize);
+        }
+    }
+}
