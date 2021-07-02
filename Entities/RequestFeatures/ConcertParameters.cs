1b0853dbd5e161ebdbfa7cfe40ccf32878ef091d
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-07-02 17:19:45 +0300

    Create ConcertParameters class for request parameters

---
+++ Entities/RequestFeatures/ConcertParameters.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Entities.RequestFeatures
+{
+    public class ConcertParameters : RequestParameters
+    {
+        public ConcertParameters()
+        {
+            OrderBy = "concertName";
+        }
+    }
+    
+
+}
