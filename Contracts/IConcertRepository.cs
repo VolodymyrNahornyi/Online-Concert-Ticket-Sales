ba5ae6bd20b486cf965c0673889c9d99613a5581
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-07-02 17:24:03 +0300

    Create IConcertRepository interface with GetConcertsAsync method

---
+++ Contracts/IConcertRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Entities.Models.Concerts;
+using Entities.RequestFeatures;
+
+namespace Contracts
+{
+    public interface IConcertRepository
+    {
+        Task<PagedList<Concert>> GetConcertsAsync(Guid genreId, Guid artistId, ConcertParameters concertParameters, bool trackChanges);
+    }
+}
