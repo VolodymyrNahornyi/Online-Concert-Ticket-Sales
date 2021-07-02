1ee2a81c8d88f0092c520297d9dda03368f2e140
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-07-02 18:10:40 +0300

    Add IConcertService with GetConcertsAsync method and implement it in ConcertService class

---
+++ Services/ConcertService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Contracts;
+using Entities.Models.Concerts;
+using Entities.RequestFeatures;
+
+namespace Services
+{
+    public class ConcertService : IConcertService
+    {
+        private readonly IRepositoryManager _repositoryManager;
+
+        public ConcertService(IRepositoryManager repositoryManager)
+        {
+            _repositoryManager = repositoryManager;
+        }
+
+        public async Task<PagedList<Concert>> GetConcertsAsync(Guid genreId, Guid artistId, ConcertParameters concertParameters, bool trackChanges)
+        {
+            return await _repositoryManager.Concert.GetConcertsAsync(genreId, artistId, concertParameters, trackChanges);
+        }
+    }
+}
