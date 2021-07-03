4ba50bf8e977c1cd2f02c9e251658826404cc7c7
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-07-03 17:06:21 +0300

    Create ConcertForManipulationDto and ConcertForCreationDto models

---
+++ Entities/DataTransferObjects/Concerts/ConcertForCreationDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Entities.Models.Concerts;
+using Entities.ValidationAttributes;
+
+namespace Entities.DataTransferObjects
+{
+    public class ConcertForCreationDto : ConcertForManipulationDto
+    {
+        
+    }
+}
---
+++ Entities/DataTransferObjects/Concerts/ConcertForManipulationDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities.DataTransferObjects
+{
+    public abstract class ConcertForManipulationDto
+    {
+        [Required(ErrorMessage = "Concert name is a required field.")]
+        [MaxLength(30, ErrorMessage = "Maximum length for the Concert name is 30 characters.")]
+        public string ConcertName { get; set; }
+        
+        [Required, DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:dd/MMM/yyyy HH:mm:ss}")]
+        public DateTime Date { get; set; }
+    }
+}
