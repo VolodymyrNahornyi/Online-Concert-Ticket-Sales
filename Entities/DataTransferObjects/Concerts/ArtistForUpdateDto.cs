ec4056d7adccc46aec373c6edc63aabbadae7fe1
User <vnagornyi1989@gmail.com>
2021-07-04 17:04:36 +0300

    Add ConcertForCreationDto collection property to the ArtistForUpdateDto class

--- Entities/DataTransferObjects/Concerts/ArtistForUpdateDto.cs
+++ Entities/DataTransferObjects/Concerts/ArtistForUpdateDto.cs
@@ -1,7 +1,9 @@
-﻿namespace Entities.DataTransferObjects
+﻿using System.Collections.Generic;
+
+namespace Entities.DataTransferObjects
 {
     public class ArtistForUpdateDto : ArtistForManipulationDto
     {
-        
+        public IEnumerable<ConcertForCreationDto> Concerts { get; set; }
     }
 }
