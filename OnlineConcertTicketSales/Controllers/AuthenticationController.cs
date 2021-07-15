d8f98991f6b7fbe74865b6d50ad327ea15ee2893
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-07-15 12:52:47 +0300

    Create AuthenticationController with RegisterUser action

---
+++ OnlineConcertTicketSales/Controllers/AuthenticationController.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using Contracts;
+using Entities.DataTransferObjects;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnlineConcertTicketSales.ActionFilters;
+
+namespace OnlineConcertTicketSales.Controllers
+{
+    [Route("api/authentication")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly ILoggerManager _logger;
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+
+        public AuthenticationController(ILoggerManager logger, IMapper mapper, UserManager<User> userManager)
+        {
+            _logger = logger;
+            _mapper = mapper;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+        public async Task<IActionResult> RegisterUser([FromBody] UserForRegistrationDto userForRegistration)
+        {
+            var user = _mapper.Map<User>(userForRegistration);
+            
+            var result = await _userManager.CreateAsync(user, userForRegistration.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.TryAddModelError(error.Code, error.Description);
+                }
+                return BadRequest(ModelState);
+            }
+
+            await _userManager.AddToRolesAsync(user, userForRegistration.Roles);
+            
+            return StatusCode(201);
+        }
+    }
+}
