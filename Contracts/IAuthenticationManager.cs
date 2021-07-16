5ba0ff6c1d8dae9bd975d163f6c624c1a5e0416c
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-07-16 15:14:55 +0300

    Create Token functionality

---
+++ Contracts/IAuthenticationManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Entities.DataTransferObjects;
+
+namespace Contracts
+{
+    public interface IAuthenticationManager
+    {
+        Task<bool> ValidateUser(UserForAuthenticationDto userForAuth);
+        Task<string> CreateToken();
+    }
+}
